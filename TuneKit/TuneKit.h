5f8deb40f7c6ff263be67944b5ecc83fd7bc6286
Timothy Moose <tmoose@vast.com>
2014-02-21 11:18:57 -0600

    Initial Commit

---
+++ TuneKit/TuneKit.h
@@ -0,0 +1,13 @@
+//
+//  TuneKit.h
+//  TuneKit
+//
+//  Created by Tim Moose on 2/21/14.
+//  Copyright (c) 2014 Tractable Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TuneKit : NSObject
+
+@end
---
+++ TuneKit/TuneKit.m
@@ -0,0 +1,13 @@
+//
+//  TuneKit.m
+//  TuneKit
+//
+//  Created by Tim Moose on 2/21/14.
+//  Copyright (c) 2014 Tractable Labs. All rights reserved.
+//
+
+#import "TuneKit.h"
+
+@implementation TuneKit
+
+@end
